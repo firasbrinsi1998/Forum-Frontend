ecdc0328ebf1a2b8031d797038dba4e5c2d4f754
Firas Brinsi <firas.brinsi@ensit.u-tunis.tn>
2022-06-30 09:41:49 +0000

    UPDATE

--- main.js
+++ main.js
@@ -13,3 +13,15 @@ function showIconBar(){
     iconbar.setAttribute ('style', "display:block;");
     navigation.classList.add('hide');
 }
+
+//Comment
+function showComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+//Reply
+function showReply(){
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
+}
